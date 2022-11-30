aaab5181948db8c10904b41ce67ec4b3c9007382
Sumeya <98185508+YA-Maya@users.noreply.github.com>
2022-11-30 13:26:11 +0000

    i have learned primitive data type and reference arrays

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-
         //this is primitive types in a Variables
         //primitive types store simple values
 
@@ -39,7 +40,6 @@ public class Main {
 /*this is a comments
 hey
  */
-
         //REFERENCE TYPES
         Date now = new Date();
         System.out.println(now);
@@ -50,8 +50,48 @@ hey
         x = 2;
         System.out.println(y);
 /*but when we use reference type there behavior is different*/
-        Point point1 = new Point();
-        Point point2 = point1;
+      //  Point point1 = new Point(x:1, y:1);
+       // Point point2 = point1;
+
+//Strings
+        String message = "i love apples";
+        System.out.println(message);
+// Concatenation
+        String message1 = "i love apples"+ "!";
+        System.out.println(message1 +"!");
+
+        //indexOf
+        String message2 = "i love apples"+ "!";
+        System.out.println(message2.indexOf("e"));
+//replace
+        String message3 = "i love apples"+ "!";
+        System.out.println(message3.replace("!", "*"));
+//Length
+        System.out.println(message3.length());
+
+//uppercase
+        System.out.println(message2.toUpperCase());
+
+//lowercase
+        System.out.println(message3.toLowerCase());
+
+//escapes sequences
+     String message4 = "Hey \"Sumeya\"";
+        System.out.println(message4);
+/*you can use \' for single quote
+\" for double quote
+\\for backslash*/
+
+//Arrays
+
+}
+public static void arrays(){
+int[] number= new int[5];
+number[0]=1;
+number[1]=2;
+
+
+    System.out.println( Arrays.toString(number));
 
 
 
@@ -59,6 +99,31 @@ hey
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }
 }
 
